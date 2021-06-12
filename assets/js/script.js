d417d1e3359b4f8933cef0c5558c5c001f05f7f5
Erin Holmes <e.holmes09@yahoo.com>
2021-06-12 13:24:53 -0500

    update audit logic and refactor js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,26 @@
-let day = moment().format("dddd, MMMM D");
-$("#currentDay").append(day);
+let currentDay = moment().format("dddd, MMMM D");
+$("#currentDay").append(currentDay);
 
-let hourlyToDos = [];
-//console.log(hourlyToDos);
-
-let workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let hourlyTasks = [];
 
 let auditTimeBlocks = function(el) {
-    let hour = $(el).find("p").text();
-    let description = $(el).find("textarea");
+    let hourEl = $(el).find("p").text();
+    let descriptionEl = $(el).find("textarea");
 
-    for (let i = 0; i < workHours.length; i++) {
-        if (moment(hour, "h" + "a").format("H") === moment().format("H")) {
-            description.addClass("present");
+    let hourBlockHour = moment(hourEl, "h" + "a").format("H");
+    let currentHour = parseInt(moment().format("H"));
+    $(".row").each(function() {
+        descriptionEl.removeClass("future past present");
+        if (hourBlockHour === moment().format("H")) {
+            descriptionEl.addClass("present");
         } 
-        //change to use isAfter
-        if (moment(hour, "h" + "a").format("H") > parseInt(moment().format("H"))) {
-            description.addClass("future");
+        if (hourBlockHour > currentHour) {
+            descriptionEl.addClass("future");
         } 
-        if (moment(hour, "h" + "a").format("H") < parseInt(moment().format("H"))) {
-            description.addClass("past");
+        if (hourBlockHour < currentHour) {
+            descriptionEl.addClass("past");
         }
-    }
+    });
 }
 
 $(".row").each(function(index, el) {
@@ -29,7 +28,6 @@ $(".row").each(function(index, el) {
 });
 
 let createToDos = function(arr, toDo) {
-    
     let rowDestination = $(".hour").filter(function() {
         return $(this).text() === toDo.hour;
     });
@@ -37,75 +35,56 @@ let createToDos = function(arr, toDo) {
 
     rowDescription.val(toDo.task);
     
-    hourlyToDos.push({
+    hourlyTasks.push({
         hour: toDo.hour,
         task: toDo.task
     });
-
-
 }
-
 
 // save button click handler
 $(".row").on("click", ".saveBtn", function() {
     let mainDiv = $(this).closest("div").attr("id");
-
     let toDoHour = $("#" + mainDiv).children()[0].innerHTML;
     let toDoTask = $("#" + mainDiv + " textarea").val();
     
-
     // if task for that day already exists, update task
-    let newArray = [];
-    $.each(hourlyToDos, function(index) {
-        let hours = hourlyToDos[index].hour;
-        
-        newArray.push(hours);
+    let tempArray = [];
+    $.each(hourlyTasks, function(index) {
+        let hours = hourlyTasks[index].hour;
+        tempArray.push(hours);
     })
-    console.log(newArray);
-     
 
-    let elIndex = newArray.indexOf(toDoHour);
-    console.log(elIndex);
-
+    let elIndex = tempArray.indexOf(toDoHour);
     if (elIndex !== -1) {
-        hourlyToDos[elIndex].task = toDoTask;
+        hourlyTasks[elIndex].task = toDoTask;
     } else {
-        hourlyToDos.push({
+        hourlyTasks.push({
             hour: toDoHour, 
             task: toDoTask
         });
-    
     }
-    saveToDos();
-    
+    saveToDos();   
 });
 
-
 let saveToDos = function() {
-    localStorage.setItem("hourlyToDos", JSON.stringify(hourlyToDos));
+    localStorage.setItem("hourlyTasks", JSON.stringify(hourlyTasks));
 };
 
 let loadToDos = function() {
-    let savedToDos = JSON.parse(localStorage.getItem("hourlyToDos"));
-
+    let savedToDos = JSON.parse(localStorage.getItem("hourlyTasks"));
     if (!savedToDos) {
         return;
     } 
     $.each(savedToDos, function(arr, toDo) {
         createToDos(arr, toDo);
-        //console.log(toDo.hour);
     })
 }
-
 loadToDos();
 
 
 setInterval(function() {
     $(".row").each(function(index, el) {
-        // let hourToAudit = $(this).children(".hour").text();
-        // let desc = $(this).children(".description");
         auditTimeBlocks(el);
-        //console.log(el);
     }) 
 }, 1000);
 
